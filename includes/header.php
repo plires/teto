4ccfad55847a6caa6b97737dd92ce80004747564
Pablo Lires <pablolires@gmail.com>
2023-01-10 18:59:30 -0300

    correccion de enlaces en header.php

--- includes/header.php
+++ includes/header.php
@@ -15,30 +15,30 @@
 					<ul>
 
 						<li>
-							<a class="<?=($current === 'home') ? 'active' : ''?> transition" href="./">Home</a>
+							<a class="<?=($current === 'home') ? 'active' : ''?> transition" href="<?= BASE ?>">Home</a>
 						</li>
 
 						<li class="<?=($current === 'productos') ? 'active' : ''?>" id="products">
 							<span id="btn_menu_products" class="transition">Productos<i id="arrow_product" class="fa-solid fa-angle-down"></i></span>
 							<ul id="content_products">
 								<li>
-									<a class="<?=($product === 'linea_origens') ? 'active' : ''?> transition" href="./linea-origenes.php">Línea Origens</a>
+									<a class="<?=($product === 'linea_origens') ? 'active' : ''?> transition" href="<?= BASE ?>linea-origenes.php">Línea Origens</a>
 								</li>
 								<li>
-									<a class="<?=($product === 'linea_plus') ? 'active' : ''?> transition" href="./linea-plus.php">Línea Plus</a>
+									<a class="<?=($product === 'linea_plus') ? 'active' : ''?> transition" href="<?= BASE ?>linea-plus.php">Línea Plus</a>
 								</li>
 								<li>
-									<a class="<?=($product === 'linea_woods') ? 'active' : ''?> transition" href="./linea-woods.php">Línea Woods</a>
+									<a class="<?=($product === 'linea_woods') ? 'active' : ''?> transition" href="<?= BASE ?>linea-woods.php">Línea Woods</a>
 								</li>						
 							</ul>
 						</li>
 
 						<li>
-							<a class="<?=($current === 'empresa') ? 'active' : ''?> transition" href="./empresa.php">Empresa</a>
+							<a class="<?=($current === 'empresa') ? 'active' : ''?> transition" href="<?= BASE ?>empresa.php">Empresa</a>
 						</li>
 
 						<li>
-							<a class="<?=($current === 'contacto') ? 'active' : ''?> transition" href="./contacto.php">Contacto</a>
+							<a class="<?=($current === 'contacto') ? 'active' : ''?> transition" href="<?= BASE ?>contacto.php">Contacto</a>
 						</li>
 
 					</ul>
