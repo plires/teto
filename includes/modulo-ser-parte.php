b415bbbef4db97cca4a71d73b398e22d10b61c13
Pablo Lires <pablolires@gmail.com>
2023-01-11 13:47:14 -0300

    Correccion de codigo en modulo-ser-parte.php

--- includes/modulo-ser-parte.php
+++ includes/modulo-ser-parte.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class="col-md-12">
 				<h2 data-aos="fade-up">¿QUERÉS SER PARTE? <br>ESCRIBÍNOS</h2>
-				<a href="<?= BASE ?>/contacto.php" class="btn btn-primary transition">CONTACTO</a>
+				<a href="<?= BASE ?>contacto.php" class="btn btn-primary transition">CONTACTO</a>
 			</div>
 		</div>
 	</div>
