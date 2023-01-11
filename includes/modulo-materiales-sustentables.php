0390862784286f5c0e95abb424ca284e826b054d
Pablo Lires <pablolires@gmail.com>
2023-01-11 16:00:04 -0300

    readecuacion de estilos para reutilizacion de modulo modulo-materiales-sustentables.php

---
+++ includes/modulo-materiales-sustentables.php
@@ -0,0 +1,22 @@
+<section class="container-fluid materiales_sustentables">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-4 content_img">
+				<img
+					data-aos="fade-up" 
+					class="img-fluid" 
+					src="<?= BASE ?>img/home/icono-materiales-sustentables.gif" 
+					alt="materiales sustentables cielorraso teto vinilico depisos">
+			</div>
+			
+			<div class="col-md-8 content_data">
+				<h2 data-aos="fade-up">MATERIALES <br><span>SUSTENTABLES</span></h2>
+				<p class="raleway_medium" data-aos="fade-up">
+					Nuestros productos son fabricados teniendo profundo respeto por el medio ambiente tanto en los procesos como en la elección de los materiales, siendo los mismos reciclables en un 100% .
+				</p>
+			</div>
+
+		</div>
+	</div>
+</section>
--- index.php
+++ index.php
@@ -224,29 +224,7 @@
 		<?php include_once('./includes/modulo-contacto.php'); ?>
 
 		<!-- Materiales Sustentables -->
-		<section class="container-fluid materiales_sustentables">
-			<div class="container">
-				<div class="row">
-
-					<div class="col-md-4 content_img">
-						<img
-							data-aos="fade-up" 
-							class="img-fluid" 
-							src="<?= BASE ?>img/home/icono-materiales-sustentables.gif" 
-							alt="materiales sustentables cielorraso teto vinilico depisos">
-					</div>
-					
-					<div class="col-md-8 content_data">
-						<h2 data-aos="fade-up">MATERIALES <br><span>SUSTENTABLES</span></h2>
-						<p class="raleway_medium" data-aos="fade-up">
-							Nuestros productos son fabricados teniendo profundo respeto por el medio ambiente tanto en los procesos como en la elección de los materiales, siendo los mismos reciclables en un 100% .
-						</p>
-					</div>
-
-				</div>
-			</div>
-		</section>
-		<!-- Materiales Sustentables end -->
+		<?php include_once('./includes/modulo-materiales-sustentables.php'); ?>
 
 		<!-- Red Distribuidores -->
 		<section class="container-fluid distribuidores">
