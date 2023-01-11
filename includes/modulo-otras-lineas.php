01bd340c7feb45937c043c7858afa6b47a28d510
Pablo Lires <pablolires@gmail.com>
2023-01-11 19:52:14 -0300

    readecuacion de codigo  para reutilizacion de modulo-otras-lineas.php

---
+++ includes/modulo-otras-lineas.php
@@ -0,0 +1,12 @@
+<section class="container-fluid otras_lineas">
+	<div class="row">
+		<div class="col-md-12">
+			<h2>OTRAS <span>LÍNEAS</span></h2>
+			<div class="content_lineas">
+				<a class="transition" href="linea-plus.php">PLUS</a>
+				<a class="transition" href="linea-origens.php">ORIGENS</a>
+				<a class="transition" href="linea-wood.php">WOOD</a>
+			</div>
+		</div>
+	</div>
+</section>
--- linea-origens.php
+++ linea-origens.php
@@ -165,19 +165,7 @@
 		<?php include_once('./includes/modulo-materiales-sustentables.php'); ?>
 
 		<!-- Otras Líneas -->
-		<section class="container-fluid otras_lineas">
-			<div class="row">
-				<div class="col-md-12">
-					<h2>OTRAS <span>LÍNEAS</span></h2>
-					<div class="content_lineas">
-						<a class="transition" href="linea-plus.php">PLUS</a>
-						<a class="transition" href="linea-origens.php">ORIGENS</a>
-						<a class="transition" href="linea-wood.php">WOOD</a>
-					</div>
-				</div>
-			</div>
-		</section>
-		<!-- Otras Líneas end -->
+		<?php include_once('./includes/modulo-otras-lineas.php'); ?>
 
 	</section>
 	<!-- Contenido PRODUCTOS end -->
