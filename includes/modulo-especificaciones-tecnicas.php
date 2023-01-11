1a15698b2c9f4cce10b5b37d0118dbdb6746ceb8
Pablo Lires <pablolires@gmail.com>
2023-01-11 20:00:44 -0300

    readecuacion de codigo  para reutilizacion de modulo-especificaciones-tecnicas.php

---
+++ includes/modulo-especificaciones-tecnicas.php
@@ -0,0 +1,47 @@
+<section class="container-fluid especificaciones">
+	
+	<div class="row">
+		<div class="col-md-12 content_title">
+			<h2 data-aos="fade-up">ESPECIFIACIONES <span>TÉCNICAS</span></h2>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12 content_img">
+				<h3 data-aos="fade-up">CORTE TRANSVERSAL</h3>
+				<img
+					data-aos="fade-up" 
+					class="img-fluid" 
+					src="<?= BASE ?>img/productos/corte-transversal.png" 
+					alt="cielorrasos teto vinilico argentina corte transversal">
+			</div>
+
+			<div class="col-sm-6 content_medidas">
+				<p data-aos="fade-up" class="jost_regular"><span class="jost_medium">MEDIDA: </span>200mmx6000mmx8mm / 200mmx3000mmx8mm</p>
+				<p data-aos="fade-up" class="jost_regular"><span class="jost_medium">PAQUETE: </span>10 placas (12 m2)</p>
+				<p data-aos="fade-up" class="jost_regular"><span class="jost_medium">MONTAJE: </span>Macho/hembra</p>
+				<p data-aos="fade-up" class="jost_regular"><span class="jost_medium">ACABADO: </span>Barniz mate</p>
+				<p data-aos="fade-up" class="jost_regular"><span class="jost_medium">ACCESORIOS: </span>Remates</p>
+			</div>
+
+			<div class="col-sm-6 content_caracteristicas">
+				<p data-aos="fade-up" class="jost_regular"><span class="jost_medium">LIMPIEZA: </span>Paño húmedo con solución de 50% água y 50% alcohol</p>
+				<p data-aos="fade-up" class="jost_regular"><span class="jost_medium">EXPOSICIÓN SOLAR: </span>No indicado</p>
+			</div>
+
+			<div class="col-md-12 content_img_medidas">
+				<img 
+					data-aos="fade-up"
+					class="img-fluid" 
+					src="<?= BASE ?>img/productos/medidas.png" 
+					alt="cielorrasos teto vinilico argentina medidas">
+			</div>
+						
+		</div>
+	</div>
+		
+	</div>
+
+</section>
